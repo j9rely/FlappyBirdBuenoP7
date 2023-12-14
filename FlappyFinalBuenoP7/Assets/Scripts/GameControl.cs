fed0b36cc73f839686dc4e1320ef181d75d98219
j9rely <145072305+j9rely@users.noreply.github.com>
2023-12-14 15:23:41 -0800

    finished rubys
    added columns and scripts

--- FlappyFinalBuenoP7/Assets/Scripts/GameControl.cs
+++ FlappyFinalBuenoP7/Assets/Scripts/GameControl.cs
@@ -3,13 +3,17 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
+using UnityEngine.UI;
 
 public class GameControl : MonoBehaviour
 {
     public static GameControl instance;
     public GameObject gameOverText;
+    public TextMeshProUGUI ScoreText;
     public bool gameOver = false;
     public float scrollSpeed = -1.5f;
+    private int score = 0;
 
     // Start is called before the first frame update
     void Awake()
@@ -29,6 +33,15 @@ public class GameControl : MonoBehaviour
         {
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
         }
+    }
+    public void BirdScored()
+    {
+        if (gameOver)
+        {
+            return;
+        }
+        score++;
+        ScoreText.text = "Score: " + score.ToString();
     }
        public void BirdDied()
     {
